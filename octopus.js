1e1625b375359d633338561732a8ac099bc74dba
maksimr <rv.maksim@gmail.com>
2011-03-12 22:56:10 +0300

    add documentation

--- octopus.js
+++ octopus.js
@@ -1,15 +1,77 @@
 /* AUTHOR:  Maksim Ryzhikov
  * NAME:    octopus
  * VERSION: 0.2
- *
- * This plugin allow you manage git's commands
- * this commands will be distributed on all directory located in *barrel* directory,
- * full path (~/.pentadactyl/plugins/barrel)
- * each of them should be under git.
- *
- * OPTIONS: directory
- *
  */
+//INFORMATION {{{
+var INFO =
+<plugin name="octopus" version="0.2"
+    href="https://github.com/maksimr/octopus"
+    summary="Octopus: gives you the ability to manage folders under git"
+    xmlns={NS}>
+    <author email="rv.maksim@gmail.com">Ryzhikov Maksim</author>
+    <license href="http://sam.zoy.org/wtfpl/">WTFPL</license>
+    <project name="Pentadactyl" minVersion="1.0"/>
+    <p>
+			This plugin allow you manage git's commands
+			this commands will be distributed on all directory located in *barrel* directory,
+			full path (~/.pentadactyl/plugins/barrel)
+			each of them should be under git.
+    </p>
+    <p>
+			If you want to use the command only for one directory,
+			you must define option -directory or shortcut -D and 
+			select the directory
+    </p>
+    <p>
+			BUGS: Yet not possible to use the command "pull",
+			you may use combination commands "fetch" and "merge" instead of "pull"
+    </p>
+
+		<p>Some available actions.
+		More information on <a>http://git-scm.com/</a>
+		</p>
+		<dl>
+			<dt>status</dt> <dd>Show the working tree status</dd>
+			<dt>diff</dt> <dd>Show changes between commits, commit and working tree, etc</dd>
+			<dt>fetch</dt> <dd>Download objects and refs from another repository</dd>
+			<dt>merge</dt> <dd>Join two or more development histories together</dd>
+			<dt>push</dt> 
+			<dd>
+				Update remote refs along with associated objects
+				Updates remote refs using local refs, while sending objects necessary to complete the given refs.
+				You can make interesting things happen to a repository every time you push into it, by setting up hooks there.
+			</dd>
+		</dl>
+		<p>FETCH</p>
+		<dl>
+			<dt>fetch</dt><dd>[options] [repository [refspec…]]</dd>
+			<dt>fetch</dt><dd>[options] group</dd>
+			<dt>fetch</dt><dd>--multiple [options>] [(repository | group)…]</dd>
+			<dt>fetch</dt><dd>--all [options]</dd>
+		</dl>
+		<p>MERGE</p>
+		<dl>
+			<dt>merge</dt><dd>git merge [-n] [--stat] [--no-commit] [--squash] [-s strategy] [-X strategy-option] [--[no-]rerere-autoupdate] [-m msg] commit… </dd>
+			<dt>merge</dt><dd>git merge msg HEAD commit… </dd>
+			<dt>merge</dt><dd>git merge --abort</dd>
+		</dl>
+
+		<p>Execute command for all folders:</p>
+		<example>git status</example>
+		<example>git fetch origin master</example>
+		<p>Fetch only for directory octopus:</p>
+		<example>git -D octopus fetch origin master</example>
+
+    <item>
+        <tags>'octopus' 'git'</tags>
+        <spec>:git status</spec>
+        <description>
+					The default action for the <ex>:git</ex> command is
+				 	check status plugin.
+        </description>
+    </item>
+</plugin>;
+//}}}
 
 var app = dactyl.plugins.app;
 
@@ -45,7 +107,7 @@ var OCTOPUS = {
 		dactyl.echomsg("OCTOPUS:" + dir.leafName.toUpperCase() + " »» " + result);
 }};
 
-group.commands.add(["octo[pus]"], "Git Mannager", function (args) {
+group.commands.add(["octopus","git"], "Git Mannager", function (args) {
 	var dirs = null;
 	if (args['-directory']||args['-D']) {
 		var dirname = args['-directory']||args['-D'], root = OCTOPUS.rootPath();
@@ -69,3 +131,5 @@ group.commands.add(["octo[pus]"], "Git Mannager", function (args) {
 		context.completions = [[dir,''] for each (dir in compl)];
 	}}]
 });
+
+// vim: set fdm=marker sw=2 ts=2 sts=2 et:
